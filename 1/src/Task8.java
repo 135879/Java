47f111dd5b84e8a33adb177594b94b2df63c9458
135879 <98060928+135879@users.noreply.github.com>
2022-01-20 14:36:20 +0300

    1

--- 1/src/Task8.java
+++
@@ -1,8 +0,0 @@
-public class Task8 {
-    public static boolean isStrangePair(String str1, String snd){
-        if (str1.isEmpty() && snd.isEmpty()) {
-            return true;
-        }
-        return str1.charAt(0) == snd.charAt(snd.length() - 1) && str1.charAt(str1.length() - 1) == snd.charAt(0);
-    }
-}
